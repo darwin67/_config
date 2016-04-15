b1592fa548f771f1b05e7ae03b222dbb4a029811
Darwin D Wu <wuddarwin@gmail.com>
2016-04-15 11:19:46 -0700

    Consolisate helm and projectile

--- emacs/helm_projectile.el
+++ emacs/helm_projectile.el
@@ -1,3 +1,31 @@
+;; ===================================================
+;;   Project management tools
+;; ===================================================
+
+;; Projectile, project management tool
+(el-get-bundle projectile)
+(projectile-global-mode)
+(setq projectile-enable-caching t)
+
+;; Ignore these directories
+(setq ignore-directories '(".git" "bundle" ".bundle" "tmp" ".sass_cache"
+			   "modules" "log" "node_modules" ".node_modules"
+			   "coverage" "vendor" "auto" "elpa" "$RECYCLE.BIN"
+			   "calendars" "system" "sandbox" "backup" ".cask"
+			   "eshell" "elpa"))
+(setq projectile-globally-ignored-directories ignore-directories)
+
+;; change root automatically when project has changed
+(setq projectile-switch-project-action 'neotree-projectile-action)
+
+;; Ignore these files
+(setq ignore-files '(".DS_Store" ".rspec" "tags" ".keep"))
+(setq projectile-globally-ignored-files ignore-files)
+
+;; Projectile-Rails
+(el-get-bundle projectile-rails)
+(add-hook 'projectile-mode-hook 'projectile-rails-on)
+
 
 ;; ===================================================
 ;;   Helm
@@ -68,6 +96,7 @@
 
 ;; helm-ls-git
 (el-get-bundle helm-ls-git)
+(global-set-key (kbd "C-c b p") 'helm-browse-project) ; Browse current project
 
 ;; helm-descbinds
 (el-get-bundle helm-descbinds)
