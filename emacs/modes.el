c901f3dc0625a0d18e55915c452e8f38912188a5
Darwin D Wu <wuddarwin@gmail.com>
2016-10-31 14:46:25 -0700

    Added coffee file ext as javascript mode

--- emacs/modes.el
+++ emacs/modes.el
@@ -39,6 +39,7 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.es6\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.coffee\\'" . js2-mode))
 (el-get-bundle json-mode)
 
 ;; vimrc
