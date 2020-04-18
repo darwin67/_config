162f205088f51fbbe78ab1006dd453851f47dccc
Darwin D Wu <darwin67@users.noreply.github.com>
2020-04-18 16:11:59 -0700

    Add custom package for buffer move

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -45,3 +45,7 @@
 ;; This is required for some packages whose default branch isn't 'master' (which
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
+
+;; Install buffer-move
+(package! buffer-move
+  :recipe (:host github :repo "lukhas/buffer-move"))
