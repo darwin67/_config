73b4c26e5841b031a736d6fb7349317ed2c5d166
Darwin D Wu <darwin67@users.noreply.github.com>
2025-01-09 22:07:10 -0800

    move zfs config

--- nixos/desktop/sophie/configuration.nix
+++ nixos/desktop/sophie/configuration.nix
@@ -6,4 +6,17 @@
     ../../common/linux/pkg.nix
     ./hardware-configuration.nix
   ];
+
+  # ZFS support
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs = {
+    forceImportRoot = false;
+    extraPools = [ "storage" ];
+  };
+
+  fileSystems."/home/darwin/storage" = {
+    device = "/mnt/storage";
+    fsType = "none";
+    options = [ "bind" ];
+  };
 }
--- nixos/desktop/sophie/hardware-configuration.nix
+++ nixos/desktop/sophie/hardware-configuration.nix
@@ -10,44 +10,33 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.luks.devices."luks-f9a7e3fb-c7e9-45d0-bc5e-b2258aefc5de".device = "/dev/disk/by-uuid/f9a7e3fb-c7e9-45d0-bc5e-b2258aefc5de";
+  boot.initrd.luks.devices."luks-f9a7e3fb-c7e9-45d0-bc5e-b2258aefc5de".device =
+    "/dev/disk/by-uuid/f9a7e3fb-c7e9-45d0-bc5e-b2258aefc5de";
   networking.hostId = "4e98920d";
   networking.hostName = "sophie"; # Define your hostname.
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules =
+    [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/14b951b4-bd5c-454d-b6fb-162741eb91ef";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/14b951b4-bd5c-454d-b6fb-162741eb91ef";
+    fsType = "ext4";
+  };
 
-  boot.initrd.luks.devices."luks-dcc35be9-edc4-4589-8dae-84d62df0012a".device = "/dev/disk/by-uuid/dcc35be9-edc4-4589-8dae-84d62df0012a";
+  boot.initrd.luks.devices."luks-dcc35be9-edc4-4589-8dae-84d62df0012a".device =
+    "/dev/disk/by-uuid/dcc35be9-edc4-4589-8dae-84d62df0012a";
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/0A56-F377";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/0A56-F377";
+    fsType = "vfat";
+    options = [ "fmask=0022" "dmask=0022" ];
+  };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/d91b2aff-8443-434e-b013-f7ed75ef9785"; }
-    ];
-
-  boot.supportedFilesystems = ["zfs"];
-  boot.zfs = {
-    forceImportRoot = false;
-    extraPools = [ "storage" ];
-  };
-
-  fileSystems."/home/darwin/storage" = {
-      device = "/mnt/storage";
-      fsType = "none";
-      options = [ "bind" ];
-  };
-
+    [{ device = "/dev/disk/by-uuid/d91b2aff-8443-434e-b013-f7ed75ef9785"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
