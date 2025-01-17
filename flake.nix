a1a9a3f3311fdd4b66009eedc6942a99aacc5f90
Darwin D Wu <darwin67@users.noreply.github.com>
2025-01-16 16:47:02 -0800

    change to use flake utils for systems

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,32 +14,32 @@
     ghostty.url = "github:ghostty-org/ghostty";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
-    let
-      username = "darwin";
-      stateVersion = "24.11";
-      wallpaperTheme = "macMonterey";
+  outputs = inputs@{ self, nixpkgs, home-manager, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystemPassThrough (system:
+      let
+        username = "darwin";
+        stateVersion = "24.11";
+        wallpaperTheme = "macMonterey";
 
-      # Function for helping configuration linux systems
-      mkLinuxSystem = { system, modules ? [ ], additionalFiles ? { } }:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            config = {
-              allowUnfree = true;
-              allowBroken = false;
-            };
-
-            overlays = [
-              inputs.timewall.overlays.default
-              inputs.ghostty.overlays.default
-
-              (final: prev: {
-                inherit (inputs.zen-browser.packages."${system}") zen-browser;
-              })
-            ];
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            allowBroken = false;
           };
-        in {
+
+          overlays = [
+            inputs.timewall.overlays.default
+            inputs.ghostty.overlays.default
+
+            (final: prev: {
+              inherit (inputs.zen-browser.packages."${system}") zen-browser;
+            })
+          ];
+        };
+
+        # Function for helping configuration linux systems
+        mkLinuxSystem = { modules ? [ ], additionalFiles ? { }, ... }: {
           specialArgs = { inherit system pkgs; };
 
           modules = modules ++ [
@@ -56,52 +57,51 @@
           ];
         };
 
-      ## list of hosts
-      hosts = {
-        sophie = mkLinuxSystem {
-          system = "x86_64-linux";
-          modules = [ ./nixos/desktop/sophie/configuration.nix ];
-          additionalFiles = {
-            ".config/sway/config.d/screen.conf" = {
-              source = "${self}/nixos/desktop/sophie/sway/screen.conf";
+        ## list of hosts
+        hosts = {
+          sophie = mkLinuxSystem {
+            modules = [ ./nixos/desktop/sophie/configuration.nix ];
+            additionalFiles = {
+              ".config/sway/config.d/screen.conf" = {
+                source = "${self}/nixos/desktop/sophie/sway/screen.conf";
+              };
+            };
+          };
+
+          xps15-7590 = mkLinuxSystem {
+            modules = [ ./nixos/laptop/xps15-7590/configuration.nix ];
+            additionalFiles = {
+              ".config/sway/config.d/screen.conf" = {
+                source = "${self}/nixos/laptop/xps15-7590/sway/screen.conf";
+              };
+            };
+          };
+
+          framework13 = mkLinuxSystem {
+            modules = [ ./nixos/laptop/framework13/configuration.nix ];
+          };
+
+          thinkpadz16 = mkLinuxSystem {
+            modules = [ ./nixos/laptop/thinkpadz16/configuration.nix ];
+            additionalFiles = {
+              ".config/sway/config.d/screen.conf" = {
+                source = "${self}/nixos/laptop/thinkpadz16/sway/screen.conf";
+              };
             };
           };
         };
-
-        xps15-7590 = mkLinuxSystem {
-          system = "x86_64-linux";
-          modules = [ ./nixos/laptop/xps15-7590/configuration.nix ];
-          additionalFiles = {
-            ".config/sway/config.d/screen.conf" = {
-              source = "${self}/nixos/laptop/xps15-7590/sway/screen.conf";
-            };
-          };
+      in {
+        # Linux setup
+        nixosConfigurations = {
+          sophie = nixpkgs.lib.nixosSystem (hosts.sophie);
+          framework = nixpkgs.lib.nixosSystem (hosts.framework13);
+          xps15-7590 = nixpkgs.lib.nixosSystem (hosts.xps15-7590);
+          ThinkpadZ16-NixOS = nixpkgs.lib.nixosSystem (hosts.thinkpadz16);
         };
 
-        framework13 = mkLinuxSystem {
-          system = "x86_64-linux";
-          modules = [ ./nixos/laptop/framework13/configuration.nix ];
-        };
+        # TODO: macOS setup
 
-        thinkpadz16 = mkLinuxSystem {
-          system = "x86_64-linux";
-          modules = [ ./nixos/laptop/thinkpadz16/configuration.nix ];
-          additionalFiles = {
-            ".config/sway/config.d/screen.conf" = {
-              source = "${self}/nixos/laptop/thinkpadz16/sway/screen.conf";
-            };
-          };
-        };
-      };
-    in {
-      # Linux setup
-      nixosConfigurations = {
-        sophie = nixpkgs.lib.nixosSystem (hosts.sophie);
-        framework = nixpkgs.lib.nixosSystem (hosts.framework13);
-        xps15-7590 = nixpkgs.lib.nixosSystem (hosts.xps15-7590);
-        ThinkpadZ16-NixOS = nixpkgs.lib.nixosSystem (hosts.thinkpadz16);
-      };
-
-      # TODO: macOS setup
-    };
+        devShells."${system}".default =
+          pkgs.mkShell { buildInputs = with pkgs; [ sops age ]; };
+      });
 }
