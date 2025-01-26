3a1825c7205d1eb76b0edbb7b766ec80a3985257
Darwin D Wu <darwin67@users.noreply.github.com>
2025-01-25 22:54:35 -0800

    auto format

--- flake.nix
+++ flake.nix
@@ -17,35 +17,37 @@
     nix-darwin = {
       url = "github:LnL7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
-};
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, flake-utils, sops-nix, nix-darwin, ... }:
-      let
-        username = "darwin";
-        stateVersion = "24.11";
-        wallpaperTheme = "macMonterey";
+  outputs = inputs@{ self, nixpkgs, home-manager, flake-utils, sops-nix
+    , nix-darwin, ... }:
+    let
+      username = "darwin";
+      stateVersion = "24.11";
+      wallpaperTheme = "macMonterey";
 
-        # Function for helping configuration linux systems
-        mkLinuxSystem = { system ? "x86_64-linux", modules ? [ ], additionalFiles ? { }, ... }: {
+      # Function for helping configuration linux systems
+      mkLinuxSystem = { system ? "x86_64-linux", modules ? [ ]
+        , additionalFiles ? { }, ... }: {
           specialArgs = { inherit system; };
 
-		pkgs = import nixpkgs {
-		  system = "x86_64-linux";
-		  config = {
-		    allowUnfree = true;
-		    allowBroken = false;
-		  };
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            config = {
+              allowUnfree = true;
+              allowBroken = false;
+            };
 
-		  overlays = [
-		    inputs.timewall.overlays.default
-		    inputs.ghostty.overlays.default
+            overlays = [
+              inputs.timewall.overlays.default
+              inputs.ghostty.overlays.default
 
-		    (final: prev: {
-		      inherit (inputs.zen-browser.packages."${system}") zen-browser;
-		    })
-		  ];
-		};
+              (final: prev: {
+                inherit (inputs.zen-browser.packages."${system}") zen-browser;
+              })
+            ];
+          };
 
           modules = modules ++ [
             sops-nix.nixosModules.sops
@@ -63,71 +65,72 @@
           ];
         };
 
-        ## list of hosts
-        hosts = {
-          sophie = mkLinuxSystem {
-            modules = [ ./nixos/desktop/sophie/configuration.nix ];
-            additionalFiles = {
-              ".config/sway/config.d/screen.conf" = {
-                source = "${self}/nixos/desktop/sophie/sway/screen.conf";
-              };
-            };
-          };
-
-          xps15-7590 = mkLinuxSystem {
-            modules = [ ./nixos/laptop/xps15-7590/configuration.nix ];
-            additionalFiles = {
-              ".config/sway/config.d/screen.conf" = {
-                source = "${self}/nixos/laptop/xps15-7590/sway/screen.conf";
-              };
-            };
-          };
-
-          framework13 = mkLinuxSystem {
-            modules = [ ./nixos/laptop/framework13/configuration.nix ];
-          };
-
-          thinkpadz16 = mkLinuxSystem {
-            modules = [ ./nixos/laptop/thinkpadz16/configuration.nix ];
-            additionalFiles = {
-              ".config/sway/config.d/screen.conf" = {
-                source = "${self}/nixos/laptop/thinkpadz16/sway/screen.conf";
-              };
+      ## list of hosts
+      hosts = {
+        sophie = mkLinuxSystem {
+          modules = [ ./nixos/desktop/sophie/configuration.nix ];
+          additionalFiles = {
+            ".config/sway/config.d/screen.conf" = {
+              source = "${self}/nixos/desktop/sophie/sway/screen.conf";
             };
           };
         };
 
-        # Linux setup
-        nixosConfigurations = if nixpkgs.stdenv.isLinux then {
-          sophie = nixpkgs.lib.nixosSystem (hosts.sophie);
-          framework = nixpkgs.lib.nixosSystem (hosts.framework13);
-          xps15-7590 = nixpkgs.lib.nixosSystem (hosts.xps15-7590);
-          ThinkpadZ16-NixOS = nixpkgs.lib.nixosSystem (hosts.thinkpadz16);
-        } else {};
-
-        # TODO: macOS setup
-        darwinConfigurations = {
-          "Darwins-Mac-mini" = nix-darwin.lib.darwinSystem {
-	    specialArgs = { inherit self inputs; };
-
-		pkgs = import nixpkgs {
-		 system = "aarch64-darwin";
-		 config.allowUnfree = true;
-};
-	    modules = [ ./nixos/desktop/m4mini/configuration.nix ];
-	  };
+        xps15-7590 = mkLinuxSystem {
+          modules = [ ./nixos/laptop/xps15-7590/configuration.nix ];
+          additionalFiles = {
+            ".config/sway/config.d/screen.conf" = {
+              source = "${self}/nixos/laptop/xps15-7590/sway/screen.conf";
+            };
+          };
         };
 
-      in {
-	inherit nixosConfigurations darwinConfigurations;
+        framework13 = mkLinuxSystem {
+          modules = [ ./nixos/laptop/framework13/configuration.nix ];
+        };
 
-        # devShells."${system}".default = pkgs.mkShell {
-        #   buildInputs = with pkgs; [
-        #     sops
-        #     age
-        #     yamllint
-        #     nodePackages.yaml-language-server
-        #   ];
-        # };
+        thinkpadz16 = mkLinuxSystem {
+          modules = [ ./nixos/laptop/thinkpadz16/configuration.nix ];
+          additionalFiles = {
+            ".config/sway/config.d/screen.conf" = {
+              source = "${self}/nixos/laptop/thinkpadz16/sway/screen.conf";
+            };
+          };
+        };
       };
+
+      # Linux setup
+      nixosConfigurations = if nixpkgs.stdenv.isLinux then {
+        sophie = nixpkgs.lib.nixosSystem (hosts.sophie);
+        framework = nixpkgs.lib.nixosSystem (hosts.framework13);
+        xps15-7590 = nixpkgs.lib.nixosSystem (hosts.xps15-7590);
+        ThinkpadZ16-NixOS = nixpkgs.lib.nixosSystem (hosts.thinkpadz16);
+      } else
+        { };
+
+      # TODO: macOS setup
+      darwinConfigurations = {
+        "Darwins-Mac-mini" = nix-darwin.lib.darwinSystem {
+          specialArgs = { inherit self inputs; };
+
+          pkgs = import nixpkgs {
+            system = "aarch64-darwin";
+            config.allowUnfree = true;
+          };
+          modules = [ ./nixos/desktop/m4mini/configuration.nix ];
+        };
+      };
+
+    in {
+      inherit nixosConfigurations darwinConfigurations;
+
+      # devShells."${system}".default = pkgs.mkShell {
+      #   buildInputs = with pkgs; [
+      #     sops
+      #     age
+      #     yamllint
+      #     nodePackages.yaml-language-server
+      #   ];
+      # };
+    };
 }
--- nixos/desktop/m4mini/configuration.nix
+++ nixos/desktop/m4mini/configuration.nix
@@ -1,31 +1,31 @@
 { pkgs, self, ... }:
 
 {
-  
-# List packages installed in system profile. To search by name, run:
-# $ nix-env -qaP | grep wget
-environment.systemPackages = with pkgs; [ 
-  vim
-neovim
-neofetch
 
-nixfmt
-];
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    vim
+    neovim
+    neofetch
 
-# Necessary for using flakes on this system.
-nix.settings.experimental-features = "nix-command flakes";
+    nixfmt
+  ];
 
-# Enable alternative shell support in nix-darwin.
-# programs.fish.enable = true;
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
 
-# Set Git commit hash for darwin-version.
-system.configurationRevision = self.rev or self.dirtyRev or null;
+  # Enable alternative shell support in nix-darwin.
+  # programs.fish.enable = true;
 
-# Used for backwards compatibility, please read the changelog before changing.
-# $ darwin-rebuild changelog
-system.stateVersion = 5;
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = self.rev or self.dirtyRev or null;
 
-# The platform the configuration will be used on.
-nixpkgs.hostPlatform = "aarch64-darwin";
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 5;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
 
 }
