b7a1f709fa184d546414755a46995623e103b6cc
Darwin Wu <darwin@Darwins-Mini.localdomain>
2025-01-25 22:44:06 -0800

    setup nix-darwin on mac mini

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Darwin's workstation setup";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
@@ -13,35 +13,39 @@
     zen-browser.url = "github:youwen5/zen-browser-flake";
     ghostty.url = "github:ghostty-org/ghostty";
     sops-nix.url = "github:Mic92/sops-nix";
+    # MacOS
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+};
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, flake-utils, sops-nix, ... }:
-    flake-utils.lib.eachDefaultSystemPassThrough (system:
+  outputs = inputs@{ self, nixpkgs, home-manager, flake-utils, sops-nix, nix-darwin, ... }:
       let
         username = "darwin";
         stateVersion = "24.11";
         wallpaperTheme = "macMonterey";
 
-        pkgs = import nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true;
-            allowBroken = false;
-          };
-
-          overlays = [
-            inputs.timewall.overlays.default
-            inputs.ghostty.overlays.default
-
-            (final: prev: {
-              inherit (inputs.zen-browser.packages."${system}") zen-browser;
-            })
-          ];
-        };
-
         # Function for helping configuration linux systems
-        mkLinuxSystem = { modules ? [ ], additionalFiles ? { }, ... }: {
-          specialArgs = { inherit system pkgs; };
+        mkLinuxSystem = { system ? "x86_64-linux", modules ? [ ], additionalFiles ? { }, ... }: {
+          specialArgs = { inherit system; };
+
+		pkgs = import nixpkgs {
+		  system = "x86_64-linux";
+		  config = {
+		    allowUnfree = true;
+		    allowBroken = false;
+		  };
+
+		  overlays = [
+		    inputs.timewall.overlays.default
+		    inputs.ghostty.overlays.default
+
+		    (final: prev: {
+		      inherit (inputs.zen-browser.packages."${system}") zen-browser;
+		    })
+		  ];
+		};
 
           modules = modules ++ [
             sops-nix.nixosModules.sops
@@ -51,7 +55,7 @@
                 useGlobalPkgs = true;
                 useUserPackages = true;
                 users.darwin = import ./nixos/common/linux/home.nix {
-                  inherit self inputs pkgs username wallpaperTheme stateVersion
+                  inherit self inputs username wallpaperTheme stateVersion
                     additionalFiles;
                 };
               };
@@ -92,24 +96,38 @@
             };
           };
         };
-      in {
+
         # Linux setup
-        nixosConfigurations = {
+        nixosConfigurations = if nixpkgs.stdenv.isLinux then {
           sophie = nixpkgs.lib.nixosSystem (hosts.sophie);
           framework = nixpkgs.lib.nixosSystem (hosts.framework13);
           xps15-7590 = nixpkgs.lib.nixosSystem (hosts.xps15-7590);
           ThinkpadZ16-NixOS = nixpkgs.lib.nixosSystem (hosts.thinkpadz16);
-        };
+        } else {};
 
         # TODO: macOS setup
+        darwinConfigurations = {
+          "Darwins-Mac-mini" = nix-darwin.lib.darwinSystem {
+	    specialArgs = { inherit self inputs; };
 
-        devShells."${system}".default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            sops
-            age
-            yamllint
-            nodePackages.yaml-language-server
-          ];
+		pkgs = import nixpkgs {
+		 system = "aarch64-darwin";
+		 config.allowUnfree = true;
+};
+	    modules = [ ./nixos/desktop/m4mini/configuration.nix ];
+	  };
         };
-      });
+
+      in {
+	inherit nixosConfigurations darwinConfigurations;
+
+        # devShells."${system}".default = pkgs.mkShell {
+        #   buildInputs = with pkgs; [
+        #     sops
+        #     age
+        #     yamllint
+        #     nodePackages.yaml-language-server
+        #   ];
+        # };
+      };
 }
